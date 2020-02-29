d09acc1ebdc3d11f06e01cd55f5aaef6090424f1
Hudson Silva de Souza <hudson@doctum.edu.br>
2020-02-28 21:56:26 -0300

    ok

--- www/js/config.js
+++ www/js/config.js
@@ -2,18 +2,21 @@ var server1 = 'https://adx.doctum.edu.br/adx/unidades/';
 var server = "https://adx.doctum.edu.br/adx/unidades/";
 
 //Nome do Aplicativo
-var appName = 'Agenda Colégio Americano' ;
+var appName = 'Agenda Escola Liber' ;
 
 //URL AUTENTICACAO
 var url_auth =  'fer/apitoken/cria_token.php';
 
 var server_action = server + 'testeretorno.php';
 
-var idonesignal = "1d5f8556-9c36-4f2e-8367-8cc21d12ae1d";
+var idonesignal = "4c03d205-c832-43ba-b816-c850b8773f04";
 var debug = 0 ;
 
 var controle_mensagem_atualizar = 1;
 
-localStorage.setItem('app_timeverify_auth', 60000);
+//Complemento para ser usado nos servicos chamados via GET
+var complemento = 'todos';
+
+localStorage.setItem('app_timeverify_auth', 180000);
 localStorage.setItem('idonesignal', idonesignal);
 localStorage.setItem('periodoletivo', '2020');
