eda167a2bfd2ba70d3e2106b2fceef335d49f2d9
Hudson Silva <hudson@doctum.edu.br>
2022-06-28 18:47:54 -0300

    novo estilo

--- www/js/view_home.js
+++ www/js/view_home.js
@@ -1,12 +1,8 @@
 function view_home(retornos){
-    let imagem = '';
 
-    if(localStorage.getItem('foto')){
-        imagem = "<img src='"+localStorage.getItem('foto')+"' width='40px'>";
-    }
     //Alterando título do app
     $('#tituloApp').html(appName);
-    $('#idNome').html(imagem+' '+localStorage.getItem("login_nome"));
+    $('#idNome').html(localStorage.getItem("login_nome"));
 
     var i ;
     var conteudo_lista = '';
@@ -19,7 +15,7 @@ function view_home(retornos){
     //Preenchendo a lista com cards
     for(i  in retorno){
 
-        conteudo_lista += "<ons-card "+(retorno[i]['url_destino'] ? "class='clicavelhome' valor='"+(retorno[i]['url_destino'])+"'" : (retorno[i]['arquivo'] ? "class='imagemview' loading='lazy' imagem='"+(retorno[i]['arquivo'])+"'" : " ")  )+"><font class='font_tam1'>"+(retorno[i]['data_inicio'])+" - "+(retorno[i]['assunto'])+"</font>";
+        conteudo_lista += "<ons-card "+(retorno[i]['url_destino'] ? "class='clicavelhome' valor='"+(retorno[i]['url_destino'])+"'" : (retorno[i]['arquivo'] ? "class='imagemview' imagem='"+(retorno[i]['arquivo'])+"'" : " ")  )+"><font class='font_tam1'><span class='notification notification--material baloes_blue'>&nbsp;&nbsp;"+(retorno[i]['data_inicio'])+"&nbsp;&nbsp;</span>  "+(retorno[i]['assunto'])+"</font>";
         conteudo_lista += "<br><br>";
         conteudo_lista += "<font class='font_text'>"+(retorno[i]['arquivo'] ? "<img width='100%' src='"+(retorno[i]['arquivo']).replace("http://", "https://")+"'><br>" : "")+quebraLinha(retorno[i]['mensagem'])+"</font>";
         conteudo_lista += "</ons-card>";
