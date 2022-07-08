93a1c7353657e567227057c0642550a0f7618e02
Hudson Silva <hudson@doctum.edu.br>
2022-07-08 16:41:10 -0300

    nova visualizacao de notas

--- platforms/android/app/src/main/assets/www/js/view_academico.js
+++ platforms/android/app/src/main/assets/www/js/view_academico.js
@@ -8,14 +8,98 @@ function view_academico(retornos){
    console.log(retornos);
 if(retornos['notas']){
     //Preenchendo a lista com cards
+    conteudo_lista += "<ons-card class='click_detalhe_nota'>";
+    conteudo_lista += "";
+    conteudo_lista += "<div style='text-align:center;width:100%'><table width='100%'><tr>";
+    conteudo_lista += "<td width='40%' align='left'><font class='font_notastam1'>Matéria/ Disciplina</font></td>";
+    conteudo_lista += "<td width='25%' align='center'><font class='font_notastam1'>Notas</font></td>";
+    conteudo_lista += "<td width='20%' align='center'><font class='font_notastam1'>Faltas</font></td>";
+    conteudo_lista += "<td width='15%' align='center'><font class='font_notastam1'>Ver</font></td>";
+    conteudo_lista += "</tr></table></div>";
+    /*conteudo_lista += "<br><font class='font_tam2'>Média da turma: " + (retorno[i]['media'])+"</font>";*/
+    conteudo_lista += "</ons-card>";
     for(i  in retorno){
         class_cor_nota =  retorno[i]['cor'] == 'danger' ? 'baloes_red' : 'baloes_blue' ;
-        conteudo_lista += "<ons-card class='click_detalhe_nota' valor='"+(i)+"'><font class='font_tam1'>"+(retorno[i]['materia'])+"</font>";
-        conteudo_lista += "<br><hr>";
-        conteudo_lista += "<br><div style='text-align:center;width:100%'><table width='100%'><tr><td width='50%' align='center'><span class='notification notification--material "+class_cor_nota+"' >&nbsp;&nbsp;Nota: " + retorno[i]['nota']+"&nbsp;&nbsp;</span></td>";
-        conteudo_lista += "<td width='50%' align='center'><span class='notification notification--material baloes_gray'>&nbsp;&nbsp;<font class='font_tam2'>Faltas: " + retorno[i]['faltas']+"&nbsp;&nbsp;</font></span></td></tr></table></div><br>";
-        /*conteudo_lista += "<br><font class='font_tam2'>Média da turma: " + (retorno[i]['media'])+"</font>";*/
-        conteudo_lista += "<div class='align_direita'><ons-button class='minhalinhadigitavel' style='font-size:10pt'><ons-icon icon='fa-list'></ons-icon> Detalhar</ons-button> </div>";
+        conteudo_lista += "<ons-card class='click_detalhe_nota' valor='"+(i)+"'>";
+        conteudo_lista += "";
+        conteudo_lista += "<div style='text-align:center;width:100%'><table width='100%'><tr>";
+        conteudo_lista += "<td width='40%' align='left'><font class='font_tam1'>"+(retorno[i]['materia'])+"</font></td>";
+        conteudo_lista += "<td width='25%' align='center'><span class='notification notification--material "+class_cor_nota+"' >&nbsp;&nbsp;" + retorno[i]['nota'].replace(".",",")+"&nbsp;&nbsp;</span></td>";
+        conteudo_lista += "<td width='20%' align='center'><span class='notification notification--material baloes_gray'>&nbsp;&nbsp;<font class='font_tam2'>" + retorno[i]['faltas']+"&nbsp;&nbsp;</font></span></td>";
+        conteudo_lista += "<td width='15%' align='center'><ons-button><ons-icon icon='md-plus-circle'></ons-icon></ons-button></td>";
+        conteudo_lista += "</tr></table></div>";
+        conteudo_lista += "</ons-card>";
+    }
+    //Exibindo os boletos
+    $('#pageNotasList').html($('#pageNotasList').html()+conteudo_lista);
+
+    //Adicionando evento click para detalhar
+    $('.click_detalhe_nota').click(function(){
+
+        var myNavigator = document.getElementById('myNavigator');
+        myNavigator.pushPage('html/page_detalhesnota.html');
+
+        //Se estiver na pagina detalhamento de botas
+    
+          dados = {
+            'apitoken': localStorage.getItem('token'),
+            'periodo_letivo': localStorage.getItem('periodoletivo')
+          }
+          if(complemento == ''){
+            agendacontrole = 0 ;
+            $('#pageAgendaList').html('');
+            $('#pageAgendaListErro').html('');
+          }
+          //Busca as notas detalhadas.. o tratamento é no view_detalhamento
+          buscaAcoes('alunos/api/notas/ver-materia/'+$(this).attr('valor')+"/",dados,'GET');
+         
+        
+        /* Aguardando carregar a pagina para configurar os eventos após os elementos estarem carregados na tela*/
+        setTimeout(function(){
+            /* Acao voltar para o app principal */
+            $('#bt_voltar_app').click(function(){
+                window.location.href = './app.html?objeto=academico';
+            });
+
+        }, 1000);
+    
+    });   
+}
+else{
+    conteudo_lista += "<ons-card><font class='font_tam1'>Nenhuma informação encontrada</font></ons-card>";
+    $('#pageNotasList').html($('#pageNotasList').html()+conteudo_lista);
+}
+}
+function view_academico(retornos){
+    var i ;
+    var conteudo_lista = '';
+    var retorno = retornos['notas'];
+    //Alterando título do app
+    
+    $('#tituloApp').html("Notas e Frequência");
+   console.log(retornos);
+if(retornos['notas']){
+    //Preenchendo a lista com cards
+    conteudo_lista += "<ons-card>";
+    conteudo_lista += "";
+    conteudo_lista += "<div style='text-align:center;width:100%'><table width='100%'><tr>";
+    conteudo_lista += "<td width='40%' align='left'><font class='font_notastam1'>Matéria/ Disciplina</font></td>";
+    conteudo_lista += "<td width='25%' align='center'><font class='font_notastam1'>Notas</font></td>";
+    conteudo_lista += "<td width='20%' align='center'><font class='font_notastam1'>Faltas</font></td>";
+    conteudo_lista += "<td width='15%' align='center'><font class='font_notastam1'>Ver</font></td>";
+    conteudo_lista += "</tr></table></div>";
+    /*conteudo_lista += "<br><font class='font_tam2'>Média da turma: " + (retorno[i]['media'])+"</font>";*/
+    conteudo_lista += "</ons-card>";
+    for(i  in retorno){
+        class_cor_nota =  retorno[i]['cor'] == 'danger' ? 'baloes_red' : 'baloes_blue' ;
+        conteudo_lista += "<ons-card class='click_detalhe_nota' valor='"+(i)+"'>";
+        conteudo_lista += "";
+        conteudo_lista += "<div style='text-align:center;width:100%'><table width='100%'><tr>";
+        conteudo_lista += "<td width='40%' align='left'><font class='font_tam1'>"+(retorno[i]['materia'])+"</font></td>";
+        conteudo_lista += "<td width='25%' align='center'><span class='notification notification--material "+class_cor_nota+"' >&nbsp;&nbsp;" + retorno[i]['nota'].replace(".",",")+"&nbsp;&nbsp;</span></td>";
+        conteudo_lista += "<td width='20%' align='center'><span class='notification notification--material baloes_gray'>&nbsp;&nbsp;<font class='font_tam2'>" + retorno[i]['faltas']+"&nbsp;&nbsp;</font></span></td>";
+        conteudo_lista += "<td width='15%' align='center'><ons-button><ons-icon icon='md-plus-circle'></ons-icon></ons-button></td>";
+        conteudo_lista += "</tr></table></div>";
         conteudo_lista += "</ons-card>";
     }
     //Exibindo os boletos
